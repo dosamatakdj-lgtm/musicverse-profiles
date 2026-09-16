cb2c3e029a5aae80021a9b4002fd2d0de0573572
dosamatakdj-lgtm <dosamatakdj@gmail.com>
2026-09-16 23:58:24 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,317 @@
+const MUSICVERSE_GROUPS = [
+
+    {
+        name: "PANDORA",
+        company: "Starship Entertainment",
+        debut: "Pandora",
+        members: [
+            "Jiwoo",
+            "Jeemin",
+            "Kylie",
+            "Hinari",
+            "Ruby",
+            "Bonnie"
+        ],
+        page: "pandora.html"
+    },
+
+    {
+        name: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        debut: "Lion",
+        members: [
+            "Ruka",
+            "Pharita",
+            "Asa",
+            "Ahyeon",
+            "Yuha",
+            "Stella",
+            "Rami",
+            "Rora",
+            "Juun",
+            "A-na",
+            "Chiquita"
+        ],
+        page: "elleven.html"
+    },
+
+    {
+        name: "PINKPUNK",
+        company: "LUX Entertainment",
+        debut: "Whistle",
+        members: [
+            "Jisoo",
+            "Jennie",
+            "Miyeon",
+            "Rosé",
+            "Mina",
+            "Lisa",
+            "Minnie"
+        ],
+        page: "pinkpunk.html"
+    },
+
+    {
+        name: "STEELROSE",
+        company: "XG Labels",
+        debut: "Jump",
+        members: [
+            "Belle",
+            "Sooin",
+            "Gawon",
+            "Garam",
+            "Anna",
+            "Ella"
+        ],
+        page: "steelrose.html"
+    }
+
+];
+
+
+const MUSICVERSE_IDOLS = [
+
+    {
+        name: "Jiwoo",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "jiwoo.jpg",
+        page: "jiwoo.html"
+    },
+
+    {
+        name: "Jeemin",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "jeemin.jpg",
+        page: "jeemin.html"
+    },
+
+    {
+        name: "Kylie",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "kylie.jpg",
+        page: "kylie.html"
+    },
+
+    {
+        name: "Hinari",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "hinari.jpg",
+        page: "hinari.html"
+    },
+
+    {
+        name: "Ruby",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "ruby.jpg",
+        page: "ruby.html"
+    },
+
+    {
+        name: "Bonnie",
+        group: "PANDORA",
+        company: "Starship Entertainment",
+        image: "bonnie.jpg",
+        page: "bonnie.html"
+    },
+
+
+    {
+        name: "Ruka",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "ruka.jpg",
+        page: "ruka.html"
+    },
+
+    {
+        name: "Pharita",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "pharita.jpg",
+        page: "pharita.html"
+    },
+
+    {
+        name: "Asa",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "asa.jpg",
+        page: "asa.html"
+    },
+
+    {
+        name: "Ahyeon",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "ahyeon.jpg",
+        page: "ahyeon.html"
+    },
+
+    {
+        name: "Yuha",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "yuha.jpg",
+        page: "yuha.html"
+    },
+
+    {
+        name: "Stella",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "stella.jpg",
+        page: "stella.html"
+    },
+
+    {
+        name: "Rami",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "rami.jpg",
+        page: "rami.html"
+    },
+
+    {
+        name: "Rora",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "rora.jpg",
+        page: "rora.html"
+    },
+
+    {
+        name: "Juun",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "juun.jpg",
+        page: "juun.html"
+    },
+
+    {
+        name: "A-na",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "a-na.jpg",
+        page: "a-na.html"
+    },
+
+    {
+        name: "Chiquita",
+        group: "ELLEVEN",
+        company: "Aurelia Entertainment",
+        image: "chiquita.jpg",
+        page: "chiquita.html"
+    },
+
+
+    {
+        name: "Jisoo",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "jisoo.jpg",
+        page: "jisoo.html"
+    },
+
+    {
+        name: "Jennie",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "jennie.jpg",
+        page: "jennie.html"
+    },
+
+    {
+        name: "Miyeon",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "miyeon.jpg",
+        page: "miyeon.html"
+    },
+
+    {
+        name: "Rosé",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "rose.jpg",
+        page: "rose.html"
+    },
+
+    {
+        name: "Mina",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "mina.jpg",
+        page: "mina.html"
+    },
+
+    {
+        name: "Lisa",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "lisa.jpg",
+        page: "lisa.html"
+    },
+
+    {
+        name: "Minnie",
+        group: "PINKPUNK",
+        company: "LUX Entertainment",
+        image: "minnie.jpg",
+        page: "minnie.html"
+    },
+
+
+    {
+        name: "Belle",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "belle.jpg",
+        page: "belle.html"
+    },
+
+    {
+        name: "Sooin",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "sooin.jpg",
+        page: "sooin.html"
+    },
+
+    {
+        name: "Gawon",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "gawon.jpg",
+        page: "gawon.html"
+    },
+
+    {
+        name: "Garam",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "garam.jpg",
+        page: "garam.html"
+    },
+
+    {
+        name: "Anna",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "anna.jpg",
+        page: "anna.html"
+    },
+
+    {
+        name: "Ella",
+        group: "STEELROSE",
+        company: "XG Labels",
+        image: "ella.jpg",
+        page: "ella.html"
+    }
+
+];
